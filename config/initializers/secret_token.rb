a1189ca94f4fe6848f6bc103834c3c9eb05af592
Alan Khara <alan.khara@hotmail.ca>
2013-05-20 17:37:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FnescPseApp::Application.config.secret_token = 'a17983d77044d1308a3d0228b423a1b62d395a9600e9f0232b3201515355e29311397202a49a83fe9cad2e1941d08792728b0bffdddca0685bcf52c56b7a6e97'
